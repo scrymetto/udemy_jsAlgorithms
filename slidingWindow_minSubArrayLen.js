4ad099106a967bf55a39cd102178fe63536eec63
hempy <hempy.scrymetto@gmail.com>
2020-02-03 11:24:56 +0300

    slidingWindow - maxSubarraySum - fail

--- slidingWindow_minSubArrayLen.js
+++
@@ -1,8 +0,0 @@
-function minSubArrayLen(arr, value) {
-    let curSum=0;
-    for (let i = 0; i<arr.length; i++){
-        if (arr[i]>=value){return 1}
-        curSum +=arr[i]
-    }
-    if(curSum<value){return 0}
-}
