49d185397a884cee00e7f78f31495ac3cd1f4a5c
hempy <hempy.scrymetto@gmail.com>
2020-02-11 14:11:20 +0300

    fib-recursion

--- recursion_fib.js
+++ recursion_fib.js
@@ -1,3 +1,20 @@
 function fib(number) {
-
+    let sum = 1;
+    let arr = [1];
+    let helper = arr => {
+        if (arr.length === number) return;
+        arr.push(arr[arr.length - 1] + (arr[arr.length - 2] || 0));
+        return helper(arr)
+    };
+    helper(arr);
+    return arr[number - 1]
 }
+
+console.log(fib(4));
+console.log(fib(28));
+
+// Solution from course:
+// function fib(n){
+//     if (n <= 2) return 1;
+//     return fib(n-1) + fib(n-2);
+// }
