10047dc2686a3ce4503c4ddd8535052ed48e3839
hempy <hempy.scrymetto@gmail.com>
2020-02-13 13:38:44 +0300

    recursion_hard - someRecursive done

--- recursion_hard_someRecursive.js
+++ recursion_hard_someRecursive.js
@@ -1,10 +1,22 @@
 function someRecursive(arr, func) {
     let result = false;
-    if (arr.length === 0) return false;
-    if (func(arr[0])){
-        result=true
+
+    function helper(array) {
+        if (array.length === 0) return;
+        if (func(array[0])) {
+            result = true;
+            return;
+        }
+        array.splice(0, 1);
+        return helper(array)
     }
-        return result !== someRecursive(arr.splice(0, 1), func)
+    helper(arr);
+    return result
 }
 
-console.log(someRecursive([4,6,8, 11], val => val > 10))
+const isOdd = val => val % 2 !== 0;
+
+console.log(someRecursive([1,2,3,4], isOdd)); // true
+console.log(someRecursive([4,6,8,9], isOdd)); // true
+console.log(someRecursive([4,6,8], isOdd)); // false
+console.log(someRecursive([4, 6, 8, 11], val => val > 10)); // true
