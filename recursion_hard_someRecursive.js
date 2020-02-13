d1c357f037970517f390cbb1a4168b758a87d6ac
hempy <hempy.scrymetto@gmail.com>
2020-02-13 13:42:50 +0300

    recursion_hard - someRecursive course solution

--- recursion_hard_someRecursive.js
+++ recursion_hard_someRecursive.js
@@ -20,3 +20,10 @@ console.log(someRecursive([1,2,3,4], isOdd)); // true
 console.log(someRecursive([4,6,8,9], isOdd)); // true
 console.log(someRecursive([4,6,8], isOdd)); // false
 console.log(someRecursive([4, 6, 8, 11], val => val > 10)); // true
+
+// Course solution:
+// function someRecursive(array, callback) {
+//     if (array.length === 0) return false;
+//     if (callback(array[0])) return true;
+//     return someRecursive(array.slice(1),callback);
+// }
