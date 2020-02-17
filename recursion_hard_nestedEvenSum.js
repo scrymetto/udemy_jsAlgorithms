22e097ce24610bd2f317b10f76fd3e5c6155c53e
hempy <hempy.scrymetto@gmail.com>
2020-02-17 12:33:09 +0300

    recursion_hard - nestedEvenSum done

--- recursion_hard_nestedEvenSum.js
+++ recursion_hard_nestedEvenSum.js
@@ -1,5 +1,14 @@
 function nestedEvenSum(obj) {
-
+    let sum = 0;
+    for (let key in obj){
+        if (typeof obj[key]==='number'&& obj[key]%2===0){
+            sum += obj[key]
+        }
+        if (typeof obj[key]==='object'){
+            sum += nestedEvenSum(obj[key])
+        }
+    }
+    return sum
 }
 
 const obj1 = {
