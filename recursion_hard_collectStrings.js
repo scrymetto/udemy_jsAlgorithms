88056292bbcef115c1d91ab713d18ede158482c2
hempy <hempy.scrymetto@gmail.com>
2020-02-17 12:38:19 +0300

    recursion_hard - collectStrings

---
+++ recursion_hard_collectStrings.js
@@ -0,0 +1,30 @@
+function collectStrings(obj) {
+    let result = [];
+    for (let key in obj){
+        if (typeof obj[key]==='string'){
+            result.push(obj[key])
+        }
+        if (typeof obj[key]==='object'){
+            result = result.concat(collectStrings(obj[key]))
+        }
+    }
+    return result
+}
+
+const obj = {
+    stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz"
+                    }
+                }
+            }
+        }
+    }
+};
+
+console.log(collectStrings(obj)); // ["foo", "bar", "baz"])
