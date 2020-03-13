adca8fdb9de37e92593fe4976577757032fa9552
hempy <hempy.scrymetto@gmail.com>
2020-03-13 12:00:20 +0300

    merge_arr with comparator fn

--- mergeSort_mergeArrays.js
+++ mergeSort_mergeArrays.js
@@ -1,14 +1,24 @@
-function mergeArrays(arr1, arr2) {
+function mergeArrays(arr1, arr2, fn) {
     let i = 0;
     let j = 0;
     let result = [];
     while (i < arr1.length && j < arr2.length) {
-        if (arr1[i] < arr2[j]) {
-            result.push(arr1[i]);
-            i++
+        if (typeof fn === 'function') {
+            if (fn(arr1[i], arr2[j]) < 0) {
+                result.push(arr1[i]);
+                i++
+            } else {
+                result.push(arr2[j]);
+                j++
+            }
         } else {
-            result.push(arr2[j]);
-            j++
+            if (arr1[i] < arr2[j]) {
+                result.push(arr1[i]);
+                i++
+            } else {
+                result.push(arr2[j]);
+                j++
+            }
         }
     }
     if (i < arr1.length && j === arr2.length) {
@@ -19,10 +29,18 @@ function mergeArrays(arr1, arr2) {
         const rest = arr2.slice(j, arr2.length);
         result = result.concat(rest)
     }
-
     return result
 }
 
-console.log(mergeArrays([1, 10, 50], [2, 14, 99, 100]));
-console.log(mergeArrays([], [2, 14, 99, 100]));
-console.log(mergeArrays([100], [2, 14, 99]));
+function comparator(str1, str2) {
+    console.log(str1, str2)
+    return str1.length - str2.length
+}
+
+const first = ['Bob', 'Ethel', 'Christine'];
+const last = ['M', 'Colt', 'Allison', 'SuperLongNameOMG'];
+
+// console.log(mergeArrays([1, 10, 50], [2, 14, 99, 100]));
+// console.log(mergeArrays([], [2, 14, 99, 100]));
+// console.log(mergeArrays([100], [2, 14, 99]));
+console.log(mergeArrays(first, last, comparator));
