7a1982d3cef22461847c9ee34c0028aa708384eb
scrymetto <hempy.scrymetto@gmail.com>
2022-02-06 19:53:39 +0300

    DLL unshift

---
+++ DLL_unshift.js
@@ -0,0 +1,49 @@
+class Node {
+  constructor(val) {
+    this.val = val
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const node = new Node(val)
+    this.length++;
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    return this;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const List = new DoublyLinkedList();
+List.push("Hello").push("Hello1").push('hello2').push("Hello3").push("Hello4");
+console.log(List);
+console.log(List.unshift('first'));
